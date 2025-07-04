a130d8147f5f22abcb09ebacabb861cb6e38ccc1
oleksandr <oleksandr@epunks.de>
2025-07-04 14:27:27 +0200

    completed up to 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    return array.filter(value => !elements.includes(value));
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
