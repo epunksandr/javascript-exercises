ecdfdf0da38ec916535d7cf0bd4fd3f294635611
oleksandr <oleksandr@epunks.de>
2025-07-04 14:28:49 +0200

    all done up 08

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+function repeatString(str, num) {
+    if (num < 0) return 'ERROR';
 
-};
+    let newString = '';
+    for (let i = 0; i < num; i++) {
+        newString += str;
+    }
 
+    return newString;
+}
+
+console.log(repeatString("abc", 2));
 // Do not edit below this line
 module.exports = repeatString;
