faa23d6a4e957f6e0fb8e9b76f060a51d1c00a22
oleksandr <oleksandr@epunks.de>
2025-07-04 12:46:47 +0200

    completes all to  07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = (string) => {
+    return string.split('').reverse().join('');
 };
+
+console.log(reverseString("Hello, World!"))
 
 // Do not edit below this line
 module.exports = reverseString;
