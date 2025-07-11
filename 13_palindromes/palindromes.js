9897fe9e71a410706b6798c2e23915360c78b590
oleksandr <oleksandr@epunks.de>
2025-07-11 14:50:29 +0200

    made 12 and 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const original = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reversed = original.split("").reverse().join("");
+    console.log(original, reversed)
+    return original === reversed;
 };
 
+console.log(palindromes("racecar!"))
 // Do not edit below this line
 module.exports = palindromes;
